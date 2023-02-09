e378efb1c0cfa415d92d924b7e41ff6eafc631cc
gokulBalaG <gokulbalaji.sjce@gmail.com>
2023-02-09 11:54:35 +0530

    Display images

--- src/AnimalShow.js
+++ src/AnimalShow.js
@@ -1,7 +1,21 @@
-function AnimalShow() {
-    return <div>
-        Cow!
-    </div>
+import cat from "./svg/cat.svg";
+import bird from "./svg/bird.svg";
+import cow from "./svg/cow.svg";
+import dog from "./svg/dog.svg";
+import gator from "./svg/gator.svg";
+import heart from "./svg/heart.svg";
+import horse from "./svg/horse.svg";
+
+const svgMap = {
+    cat, dog, cow, gator, horse, bird
+};
+
+function AnimalShow({ type }) {
+    return (
+        <div>
+            <img alt="animal" src={svgMap[type]} />
+        </div>
+    );
 }
 
 export default AnimalShow;
