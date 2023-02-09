d35d692b4dc68993be0d93f6a860586fa98fd5d3
gokulBalaG <gokulbalaji.sjce@gmail.com>
2023-02-09 12:23:22 +0530

    Add heart

--- src/AnimalShow.js
+++ src/AnimalShow.js
@@ -5,15 +5,27 @@ import dog from "./svg/dog.svg";
 import gator from "./svg/gator.svg";
 import heart from "./svg/heart.svg";
 import horse from "./svg/horse.svg";
+import { useState } from "react";
+import { click } from "@testing-library/user-event/dist/click";
 
 const svgMap = {
     cat, dog, cow, gator, horse, bird
 };
 
 function AnimalShow({ type }) {
+    const [clicks, setClicks] = useState(0);
+
+    const handleClick = () => {
+        setClicks(clicks + 1);
+    };
+
     return (
-        <div>
+        <div onClick={handleClick}>
             <img alt="animal" src={svgMap[type]} />
+            <img
+                alt="heart"
+                src={heart}
+                style={{ width: 10 + 10 * clicks + 'px' }} />
         </div>
     );
 }
